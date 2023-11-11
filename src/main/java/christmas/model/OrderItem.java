122a7a36be5373f8c72415e056fe6e6188e4258a
WooJJam <jaemin5548@naver.com>
2023-11-11 22:56:40 +0900

    [Feat] Order 클래스 일급 컬렉션 적용

--- src/main/java/christmas/model/OrderItem.java
+++ src/main/java/christmas/model/OrderItem.java
@@ -1,2 +1,19 @@
-package christmas.model;public class OrderItem {
+package christmas.model;
+
+public class OrderItem {
+    private final String menu;
+    private final int quantity;
+
+    public OrderItem(String menu, int quantity) {
+        this.menu = menu;
+        this.quantity = quantity;
+    }
+
+    public String getMenu() {
+        return menu;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
 }

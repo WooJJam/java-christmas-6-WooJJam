22037229ede89a80615a1807c96c6236cb39f7b3
WooJJam <jaemin5548@naver.com>
2023-11-12 02:40:43 +0900

    [Feat] 매뉴 카테고리 enum 생성

---
+++ src/main/java/christmas/model/Category.java
@@ -0,0 +1,32 @@
+package christmas.model;
+
+import java.util.Arrays;
+
+public enum Category {
+    APPETIZER(0),
+    MAIN_COURSE(0),
+    DESSERT(0),
+    BEVERAGE(0);
+
+    private int count;
+
+    Category(int count) {
+        this.count=count;
+    }
+
+
+    public int getCount() {
+        return count;
+    }
+
+    public static int getTotalCount() {
+        return Arrays.stream(Category.values())
+                .mapToInt(Category::getCount)
+                .sum();
+    }
+
+public void addCount() {
+        count++;
+    }
+
+}

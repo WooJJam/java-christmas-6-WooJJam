4c4f1fced0e8f8a32ff16528136caabc685ea181
WooJJam <jaemin5548@naver.com>
2023-11-11 22:57:21 +0900

    [Feat] 주문 목록 생성

--- src/main/java/christmas/model/Order.java
+++ src/main/java/christmas/model/Order.java
@@ -1,2 +1,36 @@
-package christmas.model;public class Order {
+package christmas.model;
+
+import christmas.validator.OrderItemValidate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Order {
+    private final List<OrderItem> orderItems;
+
+    public Order(String inputMenu) {
+        this.orderItems = new ArrayList<>();
+        addOrder(inputMenu);
+    }
+
+    public void addOrder(String inputMenu) {
+        OrderItemValidate.validate(inputMenu);
+
+        String INPUT_ORDER_ITEM_REGEX = "([가-힣]+)-([0-9]+)";
+        Pattern pattern = Pattern.compile(INPUT_ORDER_ITEM_REGEX);
+
+        Matcher matcher = pattern.matcher(inputMenu);
+
+        while (matcher.find()) {
+            String menu = matcher.group(1);
+            int quantity = Integer.parseInt(matcher.group(2));
+            orderItems.add(new OrderItem(menu,quantity));
+        }
+    }
+
+    public List<OrderItem> getOrderItems() {
+        return orderItems;
+    }
 }

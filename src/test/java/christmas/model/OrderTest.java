45ad4ba67ab5248c782b5a6131d78eac2c5e704d
WooJJam <jaemin5548@naver.com>
2023-11-12 02:38:27 +0900

    [Test] Order 테스트

--- src/test/java/christmas/model/OrderTest.java
+++ src/test/java/christmas/model/OrderTest.java
@@ -3,27 +3,29 @@ package christmas.model;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.util.List;
+import org.mockito.internal.matchers.Or;
 
 class OrderTest {
 
     @Test
     @DisplayName("주문한 메뉴들의 갯수가 20개를 초과하는 경우 예외가 발생한다.")
     void 주문한_메뉴들이_20개_초과일시_주문이_불가능() {
-
+        Assertions.assertThatThrownBy(() -> new Order("제로콜라-10,초코케이크-12,아이스크림-10"))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test()
     @DisplayName("주문한 메뉴가 없는 경우 예외가 발생한다.")
     void 주문한_메뉴가_존재하는지_검증() {
-
+        Assertions.assertThatThrownBy(() -> new Order("빼뺴로-3, 제로콜라-3"))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test()
     @DisplayName("음료만 주문하는 경우 예외가 발생한다.")
     void 음료만_주문하는_경우_주문이_불가능() {
-
+        Assertions.assertThatThrownBy(() -> new Order("제로콜라-1, 레드와인-1"))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test()

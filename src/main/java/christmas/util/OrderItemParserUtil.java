13fb0ad8becf4ec66e62ea055457b4c31fbfb439
WooJJam <jaemin5548@naver.com>
2023-11-12 11:02:29 +0900

    [Refactor] Order 클래스 Parser 메소드 분리

---
+++ src/main/java/christmas/util/OrderItemParserUtil.java
@@ -0,0 +1,27 @@
+package christmas.util;
+
+import christmas.constant.OrderConstant;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class OrderItemParserUtil implements OrderConstant {
+    public static List<String> parseOrderItems(String inputOrder) {
+        String formattedOrder = inputOrder.replaceAll(INPUT_ORDER_REPLACE_BLANK_REGEX, INPUT_ORDER_REPLACE_REPLACEMENT);
+        return List.of(formattedOrder.split(INPUT_ORDER_SPLIT_DELIMITER_REGEX));
+    }
+
+    public static String extractMenuName(String orderItem) {
+        return orderItem.split(INPUT_ORDER_SPLIT_HYPHEN_REGEX)[0];
+    }
+
+    public static int extractQuantity(String orderItem) {
+        return Integer.parseInt(orderItem.split(INPUT_ORDER_SPLIT_HYPHEN_REGEX)[1]);
+    }
+
+    public static List<Integer> extractQuantities(List<String> inputOrderItems) {
+        return inputOrderItems.stream()
+                .map(OrderItemParserUtil::extractQuantity)
+                .collect(Collectors.toList());
+    }
+}

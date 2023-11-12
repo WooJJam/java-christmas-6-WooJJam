3139d41c3759e601267a019438b5df787d6fcedd
WooJJam <jaemin5548@naver.com>
2023-11-12 21:08:46 +0900

    [Refactor] 할인 추상 클래스 할인 적용 메소드 삭제

--- src/main/java/christmas/model/event/Discount.java
+++ src/main/java/christmas/model/event/Discount.java
@@ -1,13 +1,7 @@
 package christmas.model.event;
 
-import christmas.model.Visit;
-import christmas.model.Week;
-
 public abstract class Discount {
     public int amount;
-
-    public abstract void applyDiscount(Visit visit);
-
 
     public int getAmount() {
         return amount;

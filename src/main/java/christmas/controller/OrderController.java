d5bc9f748e568ede3d1f6b228815138c35c27e1f
WooJJam <jaemin5548@naver.com>
2023-11-12 11:04:46 +0900

    [Refactor] 메뉴 주문 기능 service로 분리 후 Order 객체 반환

--- src/main/java/christmas/controller/OrderController.java
+++ src/main/java/christmas/controller/OrderController.java
@@ -1,25 +1,14 @@
 package christmas.controller;
 
-import camp.nextstep.edu.missionutils.Console;
 import christmas.model.Order;
 import christmas.service.OrderService;
-import christmas.view.InputView;
-import christmas.view.message.InputViewMessage;
 
 public class OrderController {
 
     private final OrderService orderService = new OrderService();
 
-    public void inputOrderMenu() {
-        try {
-            String inputOrderMenu = InputView.inputOrderMenu();
-            System.out.println("<주문 메뉴>");
-            Order order = this.orderService.createOrderMenu(inputOrderMenu);
-        } catch (IllegalArgumentException exception) {
-            System.out.println(exception.getMessage());
-            inputOrderMenu();
-        }
-
+    public Order inputOrderMenu() {
+        return this.orderService.createOrderMenu();
     }
 
 }

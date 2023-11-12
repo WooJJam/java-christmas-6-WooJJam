d8bdac64aadbf69a73d5ee6cec3121fa2a9cfb27
WooJJam <jaemin5548@naver.com>
2023-11-12 22:27:42 +0900

    [Refactor] 주말 일수 상수 list로 선언

---
+++ src/main/java/christmas/constant/DateConstant.java
@@ -0,0 +1,8 @@
+package christmas.constant;
+
+import java.util.List;
+
+public interface DateConstant {
+
+    final static List<Integer> WEEKEND_DAYS= List.of(1, 2, 8, 9, 15, 16, 22, 23, 29, 30);
+}

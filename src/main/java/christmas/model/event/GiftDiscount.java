43ef92473c24f2d968747936aa028e8b0e166a43
WooJJam <jaemin5548@naver.com>
2023-11-13 17:48:40 +0900

    [Feat] 증정 이벤트 model 구현

--- src/main/java/christmas/model/event/GiftDiscount.java
+++ src/main/java/christmas/model/event/GiftDiscount.java
@@ -1,4 +1,20 @@
 package christmas.model.event;
 
-public class GiftDiscount {
+import christmas.model.Menu;
+import christmas.model.Order;
+
+public class GiftDiscount extends Discount {
+
+    public GiftDiscount(Order order) {
+        this.amount = applyDiscount(order);
+    }
+
+    private int applyDiscount(Order order) {
+        int totalOrderAmount = order.getAmount();
+
+        if (totalOrderAmount >= 120000){
+            return Menu.CHAMPAGNE.getPrice();
+        }
+        return 0;
+    }
 }
